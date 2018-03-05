d5a0a34fa33f648633ed4afeb81f5b5d31e374d9
Uram <215054218@student.uj.ac.za>
2018-03-05 06:45:50 +0200

    Logi
    Login page

---
+++ Login/LoginPage.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginPage.aspx.cs" Inherits="Login.LoginPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login</title>
+    <style type="text/css">
+        html {
+            background-color:blue;
+        }
+        .content {
+            padding:10px;
+            margin:0px auto;
+            align-content:center;
+            width:200px;
+            background:white;
+        }
+
+        .inputs {
+            padding:2px;
+            margin-bottom: 16px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="content">
+        <h3><u>Login or Register</u></h3>
+        <asp:TextBox
+            id="txtUserName"
+            PlaceHolder="User name"
+            cssClass="inputs"
+            runat="server" Height="30px" Width="193px" />
+        <br />
+        <asp:TextBox
+            id="txtPass"
+            PlaceHolder="Password"
+            TextMode="Password"
+            cssClass="inputs"
+            runat="server" Height="32px" Width="196px" />
+        <br />
+        <asp:Button
+            id="btnLogin"
+            Text="Login"
+            cssClass="inputs"
+            width="100%"
+            runat="server" Height="35px" OnClick="btnLogin_Click1" />
+           &nbsp;
+        
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:UsersConnectionString %>" ProviderName="<%$ ConnectionStrings:UsersConnectionString.ProviderName %>" SelectCommand="SELECT * FROM [User]"></asp:SqlDataSource>
+        <br /><br />
+        <asp:Label
+            id="lblResults"
+            Text="uytiuiyiu"
+            
+            width="25%"
+            height="30%"
+            runat="server" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Login/LoginPage.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace Login
+{
+
+    public partial class LoginPage : System.Web.UI.Page
+    {
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUserName.Text))
+            {
+                txtUserName.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtPass.Text))
+            {
+                lblResults.Text = "Please type your Password";
+                txtPass.Focus();
+                return;
+            }
+
+            try
+            {
+                string constring = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\U\Documents\Login\Login\Users.accdb;";
+                OleDbConnection conDataBase = new OleDbConnection(constring);
+                OleDbCommand cmdDataBase = new OleDbCommand("Select * from [User] where UserName='"
+                    + this.txtUserName.Text
+                    + "' and UserPass='"
+                    + this.txtPass.Text
+                    + "';", conDataBase);
+                OleDbDataReader myReader;
+
+                conDataBase.Open();
+                myReader = cmdDataBase.ExecuteReader();
+
+                int count = 0;
+
+                while (myReader.Read())
+                {
+                    count = count + 1;
+                }
+                if (count == 1)
+                {
+                    lblResults.Text = "Login Successful";
+                }
+                else if (count > 1)
+                {
+                    lblResults.Text = "Duplicate Username or Password";
+                }
+                else
+                {
+                    lblResults.Text = "Username or Password do not match";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblResults.Text = ex.Message;
+            }
+        }
+    }
+}
